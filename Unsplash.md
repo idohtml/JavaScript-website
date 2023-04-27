b15a51ca5c005a6dc20cdc76a06fee14c81f1678
Jacob <Jacob.K289@outlook.com>
2023-04-27 23:46:12 +0200

    done for now

--- Unsplash.md
+++
@@ -1,3 +0,0 @@
-License for the header image can be seen here: https://unsplash.com/license
-
-Other images are taken from my company Apolloz Fotografi AB. As the Co-owner of the company i have all rights reserved and therefore don't break any rules.
--- js/index.js
+++ js/index.js
@@ -12,3 +12,17 @@ heroText.innerHTML = "The story of my journey must continue";
 beatTitle.innerHTML = "Welcome to my world";
 faqTitle.innerHTML = "Frequently Asked Questions";
 galleryTitle.innerHTML = "Gallery";
+
+// BLOB
+const blob = document.getElementById("blob");
+document.body.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: "forwards" }
+  );
+};
